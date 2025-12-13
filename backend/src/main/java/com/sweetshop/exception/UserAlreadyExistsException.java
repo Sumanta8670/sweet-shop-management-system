9ba6751148e97d5e1626179e7e4e8bdcf6e80e01
Sumanta Jana <janasumanta59@gmail.com>
2025-12-13 13:28:31 +0530

    feat: Create UserAlreadyExistsException

---
+++ backend/src/main/java/com/sweetshop/exception/UserAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.sweetshop.exception;
+
+/**
+ * Exception thrown when attempting to register a user that already exists.
+ */
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
