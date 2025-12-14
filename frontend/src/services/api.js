3a5c7e2154f57ca47ea22e61858f53303c20b412
Sumanta Jana <janasumanta59@gmail.com>
2025-12-14 15:39:17 +0530

    fix: Fix api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://sweet-shop-api-glak.onrender.com/api';
 
 /**
  * Create an Axios instance with base configuration
